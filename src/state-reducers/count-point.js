e48afff72eaaddee1a621415cc7ba557c28591be
hejny <me@pavolhejny.com>
2017-06-24 08:02:43 +0200

    Github pages

--- src/state-reducers/count-point.js
+++ src/state-reducers/count-point.js
@@ -1,22 +1,22 @@
 const defaultCountPoint = `(t, u, v)=>{
-    const u1 = u - 10;
-    const v1 = v - 10;
+const u1 = u - 10;
+const v1 = v - 10;
 
-    const u2 = u + 10;
-    const v2 = v + 10;
+const u2 = u + 10;
+const v2 = v + 10;
 
-    const distance1 = Math.sqrt(u1 * u1 + v1 * v1);
+const distance1 = Math.sqrt(u1 * u1 + v1 * v1);
 
-    const m = 1.5;
-    const top = (Math.cos(v * 3 + t * 3) + Math.cos(u * 5 + t * 7) + 1) / 10 + 1;
+const m = 1.5;
+const top = (Math.cos(v * 3 + t * 3) + Math.cos(u * 5 + t * 7) + 1) / 10 + 1;
 
-    return ([
-        -10 * top * Math.cos(u) * (m + Math.cos(v))
-        ,
-        -10 * top * Math.sin(v)
-        ,
-        -10 * top * Math.sin(u) * (m + Math.cos(v))
-    ]);
+return ([
+-10 * top * Math.cos(u) * (m + Math.cos(v))
+,
+-10 * top * Math.sin(v)
+,
+-10 * top * Math.sin(u) * (m + Math.cos(v))
+]);
 }`;
 
 
